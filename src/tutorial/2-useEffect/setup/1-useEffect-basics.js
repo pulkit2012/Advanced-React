d23d22a9d7ec228b859930690273c30301a4db3c
Pulkit Varshney <pulkitvarshney2012@gmail.com>
2021-06-10 13:24:41 +0530

    Finished till Short-Ckts

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 // by default runs after every re-render
 // cleanup function
 // second parameter
-const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
 
-export default UseEffectBasics;
+const UseEffectBasics = () => {
+  const [value, setValue] = useState(0)
+  //this useEffect will run after every re-render but cuz of condition it will run after 0
+  useEffect(() => {
+    if (value > 0) {
+      document.title = `new Message ${value}`
+    }
+  }, []) // by adding [] we specified yhat it will run only on initial render
+  useEffect(() => {
+    console.log('I dont know')
+  }, [value]) // by adding [value] we specified it will run only after value is changed render
+  useEffect(() => {
+    console.log('Hey')
+  })
+  return (
+    <>
+      <h1>{value}</h1>
+      <button
+        className='btn'
+        onClick={() => {
+          setValue(value + 1)
+        }}
+      >
+        Clicke here
+      </button>
+    </>
+  )
+}
+
+export default UseEffectBasics
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
-};
+  const [size, setSize] = useState(window.innerWidth)
 
-export default UseEffectCleanup;
+  const valuechanger = () => {
+    setSize(window.innerWidth)
+  }
+  // as we render every time the screen changes we got so many event listeners and eventually we got a memory leak  to solve this we have to cleanup prev calls
+  useEffect(() => {
+    window.addEventListener('resize', valuechanger)
+    /* return () => {
+      // cleanup function
+      window.removeEventListener('resize', valuechanger)
+    } */
+  }, []) // we can also add [] to solve prob but it will not work for most of the cases
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{size} px</h2>
+    </>
+  )
+}
+
+export default UseEffectCleanup
