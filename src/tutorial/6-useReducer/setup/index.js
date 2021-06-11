106ed3713a4bbb77e8a0b8c75a9f6b32b977ec38
Pulkit Varshney <pulkitvarshney2012@gmail.com>
2021-06-11 13:31:35 +0530

    Use Reducer started

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,47 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [modal, setModal] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      setModal(true)
+      setPeople([
+        ...people,
+        { id: new Date().getTime().toString(), name: name },
+      ])
+      setName('')
+    } else {
+      setModal(true)
+    }
+  }
+  return (
+    <>
+      {modal && <Modal />}
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type='submit'> add people</button>
+      </form>
+      {people.map((person) => {
+        return (
+          <div key={person.id}>
+            <h4>{person.name}</h4>
+          </div>
+        )
+      })}
+    </>
+  )
+}
 
-export default Index;
+export default Index
